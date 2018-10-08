2bd7eb2c0e8491164ae2c01966585e57eb375dfc
ntalcus <alcus.noah@gmail.com>
2018-10-08 11:00:14 -0700

    prelim work on a max string fn

--- event-scraper.py
+++ event-scraper.py
@@ -5,18 +5,24 @@ from lxml import html
 
 url = "https://eecs.berkeley.edu/turing-colloquium/schedule" 
 
+def maxlength(strList):
+    maxList = None
+    maxLength = 0
+    for string in strList:
+
+
 page = requests.get(url)
 tree = html.fromstring(page.content)
 speakers = []
 titles = []
 dates = []
 for i in range(0,12):
-    speaker_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/h3[' + str(i) + ']/a/text()'
+    speaker_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/h3[' + str(1 + i) + ']/a/text()'
     speakers.append(tree.xpath(speaker_xpath))
-    title_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/h4[' + str(i) + ']/strong/a/text()'
+    title_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/h4[' + str(1 + i) + ']/strong/a/text()'
     title = tree.xpath(title_xpath)
     if (len(title) == 0):
-        title_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/h4[' + str(i) + ']/strong/text()'
+        title_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/h4[' + str(1 + i) + ']/strong/text()'
         title = tree.xpath(title_xpath)
         print("triggered", title)
     titles.append(title)
@@ -26,6 +32,8 @@ for i in range(0,12):
         date_xpath = '//*[@id="block-system-main"]/div/div/section/div[1]/div[1]/div/div/div/div/p[' + str(3 + i) + ']/strong[1]/text()'
         date = tree.xpath(date_xpath)
     dates.append(date)
-print("speakers: ", speakers)
-print("titles: ", titles)
-print("dates: ", dates)
+for i in range(0,12):
+    print("lecture # ", str(i))
+    print("speakers: ", speakers[i])
+    print("titles: ", titles[i])
+    print("dates: ", dates[i])
